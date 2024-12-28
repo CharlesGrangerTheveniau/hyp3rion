568bf7ca961c45dbb589eab90740f7d9f01dfe08
Charles Theveniau <charlestheveniau@gmail.com>
2024-12-28 22:21:41 +0100

    FE: create userStore using pinia

---
+++ app/pages/client/[id]/index.vue
@@ -0,0 +1,29 @@
+<!-- eslint-disable vue/no-multiple-template-root -->
+<template>
+    <h1>Client Dashboard</h1>
+    
+</template>
+
+<script lang="ts" setup>
+
+
+definePageMeta({
+  layout: 'dashboard'
+})
+
+const user = useSupabaseUser()
+console.log(user.value)
+
+
+if (!user.value?.id) {
+  navigateTo('/login')
+}
+
+
+
+
+</script>
+
+<style>
+
+</style>
---
+++ app/pages/firm/[id]/dashboard.vue
@@ -0,0 +1,24 @@
+<!-- eslint-disable vue/no-multiple-template-root -->
+<template>
+    <h1>Firm Dashboard</h1>
+    
+</template>
+
+<script lang="ts" setup>
+
+
+definePageMeta({
+  layout: 'dashboard'
+})
+
+const user = useSupabaseUser()
+console.log(user.value)
+
+
+
+
+</script>
+
+<style>
+
+</style>
